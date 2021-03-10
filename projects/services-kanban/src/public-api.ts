1fbed2170a82d5c8438b6ab608a5f76f9388cfa1
= <=>
2021-03-10 19:05:37 +0100

    0.0.5

--- projects/services-kanban/src/public-api.ts
+++ projects/services-kanban/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of services-kanban
  */
 
-export * from './lib/services-kanban.service';
 export * from './lib/services-kanban.component';
 export * from './lib/services-kanban.module';
 
